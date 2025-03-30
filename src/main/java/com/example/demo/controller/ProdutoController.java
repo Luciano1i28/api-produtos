af88d77a1f4ad065b76e4fbe1c50e248e6a17dee
Luciano1i28 <lucianomoronadecampos@gmail.com>
2025-03-30 19:32:06 -0300

    Versão corrigida do projeto com jakarta e ajustes finais

--- src/main/java/com/example/demo/controller/ProdutoController.java
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.Produto;
 import com.example.demo.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 import java.util.List;
 
